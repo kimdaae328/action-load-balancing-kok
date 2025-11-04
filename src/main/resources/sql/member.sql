1345144e40605cde29eabac46bcecabb9eb7e9b6
kimdaae <kimdaae328@gmail.com>
2025-11-04 11:13:55 +0900

    init

---
+++ src/main/resources/sql/member.sql
@@ -0,0 +1,17 @@
+create type provider as enum('google', 'kakao', 'naver', 'kok');
+
+create table tbl_member
+(
+    user_id            bigint                           not null
+        primary key
+        constraint fk_member_user
+            references tbl_user,
+    member_provider    provider default 'kok'::provider not null,
+    member_profile_url varchar(255),
+    member_info        text
+);
+
+select * from tbl_member;
+
+DELETE FROM tbl_member
+WHERE user_id = 4;
